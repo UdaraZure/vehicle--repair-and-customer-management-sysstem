9478ede1101feb7f94f4e02e201b578e4453ad0d
UdaraZure <88130450+UdaraZure@users.noreply.github.com>
2023-05-23 20:48:55 +0530

    Initiating

---
+++ project1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CollapsibleExample from './components/Navibar';
+
+function App() {
+  return (
+    <div className="App">
+      <CollapsibleExample/>
+    </div>
+  );
+}
+
+export default App;
