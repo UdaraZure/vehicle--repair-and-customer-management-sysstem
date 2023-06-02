cf766434c7ca5ab4c352c70e97795dbdb68b7d2e
UdaraZure <88130450+UdaraZure@users.noreply.github.com>
2023-06-02 20:12:02 +0530

    Update

--- project1/src/components/Navibar.jsx
+++ project1/src/components/Navibar.jsx
@@ -1,32 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GarageIcon from "./GarageIcon.png"
 
 function Navigationbar() {
   return (
     <>
     <div>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed='top' >
+    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark" fixed='top' >
       <Container>
-        
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/">
+              <img className='GarageIcon'
+              src={GarageIcon}
+              height="50"
+              width = "120"
+              />
+            </Nav.Link>
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/Offers">Offers</Nav.Link>
+            <Nav.Link href="/ContactUs">Contact Us</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="/login" className="Login-button">Login</Nav.Link>
