dc5613b05b5175c3b033262f5874021d72d827d3
UdaraZure <88130450+UdaraZure@users.noreply.github.com>
2023-06-05 18:25:18 +0530

    update

--- project1/src/Pages/Signup/Signup.jsx
+++ project1/src/Pages/Signup/Signup.jsx
@@ -1,23 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
 import './signupPge.css'
 
-export const Header = () => {
+export const SignUp = () => {
+  const [firstName, setFirstName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (firstName.trim() === '') {
+      alert('First name cannot be empty');
+      return;
+    }
+    // Proceed with form submission if validation passes
+  };
   return (
     <>
     <div className='form' style={{marginTop:"25vh"}}>
     <div className="form-body">
         <h4 style={{textAlign: 'center', fontWeight:'1000'}}> Sign Up </h4>
+        <form onSubmit={handleSubmit}>
               <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
+              <label className="form__label" htmlFor="firstName">First Name </label>
+              <input
+                className="form__input"
+                type="text"
+                id="firstName"
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
               </div>
-              <div className="lastname">
+              </form>
+              <div className="lastname"> 
                   <label className="form__label" for="lastName">Last Name </label>
                   <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
               </div>
               <div className="email">
                   <label className="form__label" for="email">Email </label>
                   <input  type="email" id="email" className="form__input" placeholder="Email"/>
+              </div>
+              <div className="mobileNum">
+                  <label className="form__label" for="mobileNum">Mobile Number </label>
+                  <input  type='number' id="mobileNum" className="form__input" placeholder="mobile Number"/>
               </div>
               <div className="password">
                   <label className="form__label" for="password">Password </label>
@@ -28,11 +53,13 @@ export const Header = () => {
                   <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
               </div>
           </div>
+
           <div class="footer">
               <button type="submit" class="btn">Submit</button>
           </div>
+          
     </div>
     </>
   )
 }
-export default Header;
+export default SignUp;
