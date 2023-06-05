76e5d091fee4e5b86e89b8658e6152b1dc930fa6
UdaraZure <88130450+UdaraZure@users.noreply.github.com>
2023-06-05 19:35:40 +0530

    update

--- project1/src/Pages/Signup/Signup.jsx
+++ project1/src/Pages/Signup/Signup.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
-import './signupPge.css'
+import React, { useState } from 'react';
+import './signupPge.css';
 
 export const SignUp = () => {
   const [firstName, setFirstName] = useState('');
+  const [firstNameError, setFirstNameError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
     if (firstName.trim() === '') {
-      alert('First name cannot be empty');
+      setFirstNameError('The first name shouldn\'t be null');
       return;
     }
+
     // Proceed with form submission if validation passes
+    // TODO: Handle form submission logic here
   };
+
   return (
     <>
-    <div className='form' style={{marginTop:"25vh"}}>
-    <div className="form-body">
-        <h4 style={{textAlign: 'center', fontWeight:'1000'}}> Sign Up </h4>
-        <form onSubmit={handleSubmit}>
-              <div className="username">
+      <div className='form' style={{ marginTop: '25vh' }}>
+      <form onSubmit={handleSubmit}>
+        <div className="form-body">
+          <h4 style={{ textAlign: 'center', fontWeight: '1000' }}> Sign Up </h4>
+          
+            <div className="username">
               <label className="form__label" htmlFor="firstName">First Name </label>
               <input
                 className="form__input"
@@ -28,10 +32,13 @@ export const SignUp = () => {
                 id="firstName"
                 placeholder="First Name"
                 value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                  setFirstNameError('');
+                }}
               />
-              </div>
-              </form>
+              {firstNameError && <p className="error-message">{firstNameError}</p>}
+            </div>
               <div className="lastname"> 
                   <label className="form__label" for="lastName">Last Name </label>
                   <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
@@ -52,14 +59,15 @@ export const SignUp = () => {
                   <label className="form__label" for="confirmPassword">Confirm Password </label>
                   <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
               </div>
-          </div>
-
-          <div class="footer">
-              <button type="submit" class="btn">Submit</button>
-          </div>
+              <div className="footer">
+              <button type="submit" className="btn">Submit</button>
+            </div>
           
-    </div>
+        </div>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
+
 export default SignUp;
